14f1a8903c6f1063998435b3a8c96e69f5afcb1d
Chai <isaigomezhernandez.com>
2019-12-12 16:25:17 -0600

    Funciona el calendar

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
 import React, { Component } from "react";
-import * as RM from "react-native";
 import Mycalendar from "./component/Mycalendar";
-import { StyleSheet, Text, View } from "react-native";
+
+import { View } from "react-native";
 
 export default class App extends Component {
   render() {
     return (
-      <RM.View style={styles.container}>
-        <Mycalendar></Mycalendar>
-      </RM.View>
+      <View>
+        <Mycalendar />
+      </View>
     );
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+const style = {};
